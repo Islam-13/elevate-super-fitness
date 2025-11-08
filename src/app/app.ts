2e9fd302f6ed0e691ad6f1348750e6b0d57be054
Islam-13 <islam-hussein@outlook.com>
2025-11-08 15:40:55 +0300

    remove testing in app html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Banner } from './shared/components/ui/banner/banner';
 
 @Component({
-  imports: [RouterModule, Banner],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'elevate-super-fitness';
-  // themeService = inject(ThemeService);
-
-  // toggleTheme() {
-  //   this.themeService.toggleTheme();
-  // }
+  protected title = 'Super Fitness';
 }
