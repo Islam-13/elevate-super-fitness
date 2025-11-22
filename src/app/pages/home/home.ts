0e47fda56107c8badb371b959c99470ffebf95f0
ayayahyaa <ayayahya232@gmail.com>
2025-11-22 04:18:48 +0200

    fix: workouts section fixes

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Hero } from "../../features/hero/hero";
+import { AboutAs } from "../../shared/components/ui/about-us/aboutAs";
+import { HealthyNutritions } from "../../features/meals-categories/components/healthyNutritions";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [AboutAs, HealthyNutritions,Hero],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
--- src/app/shared/components/ui/section-title/sectionTitle.ts
+++ src/app/shared/components/ui/section-title/sectionTitle.ts
@@ -8,6 +8,11 @@ import { Component, input, InputSignal } from '@angular/core';
 })
 export class SectionTitle {
   title: InputSignal<string> = input.required<string>();
+<<<<<<< HEAD
   subtitle: InputSignal<string> = input.required<string>();
 
+=======
+  subTitle: InputSignal<string> = input.required<string>();
+  subTitlePositionStart: InputSignal<boolean> = input.required<boolean>();
+>>>>>>> 6ac2fc41c99b8371458967a0bc50743e270359e2
 }
