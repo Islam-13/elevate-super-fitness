397d4047d5768173451c931022762936c59b6080
ayayahyaa <ayayahya232@gmail.com>
2025-11-24 19:46:40 +0200

    new commit

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
 import { Hero } from "../../features/hero/hero";
-<<<<<<< HEAD
-import { AboutAs } from "../../shared/components/ui/about-us/aboutAs";
-import { HealthyNutritions } from "../../features/meals-categories/components/healthyNutritions";
-=======
-import { AboutAs } from '../../shared/components/ui/about-us/about-as';
-import { HealthyNutritions } from '../../features/meals-categories/components/healthy-nutritions';
->>>>>>> 66931d89b1c8456af7af1c029a2bb0df122bd7e6
 import { Workouts } from "../../features/workouts/workouts";
 
 @Component({
   selector: 'app-home',
-  imports: [AboutAs, HealthyNutritions, Hero, Workouts],
+  imports: [Hero, Workouts],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
--- src/app/shared/components/business/taps/taps.ts
+++ src/app/shared/components/business/taps/taps.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { Component, EventEmitter,  Input,  Output, signal,  } from '@angular/core';
-=======
-import { Component, EventEmitter, inject, Input, OnInit, Output, signal,  } from '@angular/core';
->>>>>>> 66931d89b1c8456af7af1c029a2bb0df122bd7e6
+import { Component, EventEmitter, Input,Output, signal,  } from '@angular/core';
 import { MusclesGroup } from '../../../interfaces/muscles-groups';
 
 @Component({
@@ -16,16 +12,9 @@ export class Taps {
   @Input() muscleGroups = signal<MusclesGroup[]>([]);
   @Input() selectedGroupId = signal<string | null>(null);
   @Output() groupSelected = new EventEmitter<string>();
-<<<<<<< HEAD
 
-=======
->>>>>>> 66931d89b1c8456af7af1c029a2bb0df122bd7e6
   selectGroup(groupId: string) {
     this.selectedGroupId.set(groupId);
     this.groupSelected.emit(groupId);
   }
-<<<<<<< HEAD
-
-=======
->>>>>>> 66931d89b1c8456af7af1c029a2bb0df122bd7e6
 }
