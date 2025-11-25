63694a11a09c7d3716b24f6ca5aa23f480de37ab
FatmaElhosary <engfatmafathy@gmail.com>
2025-11-25 18:48:26 +0300

    finished details design

---
+++ src/app/features/meals-categories/components/ui/healthy-details/healthy-details.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NgOptimizedImage } from "@angular/common";
+
+@Component({
+  selector: 'app-healthy-details',
+  imports: [NgOptimizedImage],
+  templateUrl: './healthy-details.html',
+  styleUrl: './healthy-details.scss',
+})
+export class HealthyDetails {}
