64412be3cfc16e87178c6135059eb8433d7e4864
FatmaElhosary <engfatmafathy@gmail.com>
2025-11-27 11:17:28 +0300

    fixed -meals -taps

--- src/app/features/meals-categories/components/ui/meals-group-taps/taps.ts
+++ src/app/features/meals-categories/components/ui/meals-group-taps/taps.ts
@@ -1,19 +1,46 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MealsCategoryDTO } from '../../../../../shared/types/mealCategory.interface';
-
+import { CarouselModule } from 'primeng/carousel';
 @Component({
   selector: 'app-taps',
   standalone: true,
   templateUrl: './taps.html',
   styleUrls: ['./taps.scss'],
-  imports: [],
+  imports: [CarouselModule],
 })
-export class Taps {
+export class Taps implements OnInit {
   @Input() mealsCategories: MealsCategoryDTO[];
-  @Input() selectedGroupId :string;
+  @Input() selectedGroupId: string;
   @Output() groupSelected = new EventEmitter<string>();
+  responsiveOptions: any[] | undefined;
+
   selectGroup(groupId: string) {
-    this.selectedGroupId=groupId;
+    this.selectedGroupId = groupId;
     this.groupSelected.emit(groupId);
+  }
+
+  ngOnInit(): void {
+    this.responsiveOptions = [
+      {
+        breakpoint: '1400px',
+        numVisible: 4,
+        numScroll: 3,
+      },
+      {
+        breakpoint: '1199px',
+        numVisible: 3,
+        numScroll: 3,
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 4,
+        numScroll: 3,
+      },
+      {
+        breakpoint: '575px',
+        numVisible: 4,
+        numScroll: 3,
+      },
+    ];
   }
 }
