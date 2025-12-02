9e243f3bb8adf3945560d46b7a2293e062cdbfb2
Ahmed-Osama-Elfeky <elfekya166@gmail.com>
2025-12-02 08:33:32 +0200

    Chatbot is ready

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,30 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Navbar } from "./shared/components/ui/navbar/navbar";
 import { AppLayout } from "./layouts/app-layout/appLayout";
+import { PrimaryBtn } from "./shared/components/ui/primary-btn/primaryBtn";
+
+import { CommonModule } from '@angular/common';
+import { Chatbot } from "./features/chatbot/chatbot";
 
 @Component({
-  imports: [RouterModule, Navbar, AppLayout],
+  imports: [
+    CommonModule,
+    RouterModule,
+    Navbar,
+    AppLayout,
+    PrimaryBtn,
+    Chatbot
+],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected title = 'Super Fitness';
+  isChatOpen = false;
+
+toggleChat() {
+  this.isChatOpen = !this.isChatOpen;
+}
+
 }
--- src/app/layouts/app-layout/appLayout.ts
+++ src/app/layouts/app-layout/appLayout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Home } from "../../pages/home/home";
+import { About } from "../../pages/about/about";
 
 @Component({
   selector: 'app-app-layout',
-  imports: [ Home],
+  imports: [Home, About],
   templateUrl: './appLayout.html',
   styleUrl: './appLayout.scss',
 })
