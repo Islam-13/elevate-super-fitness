c0383dde1d13e5d7890dd49ca1fed0f680b2efde
Islam-13 <islam-hussein@outlook.com>
2026-01-27 01:01:49 +0300

    fix auth comments

--- src/app/features/pages/about/about.ts
+++ src/app/features/pages/about/about.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 import { Aboutus } from '@shared/components/ui/about-us/about-us';
-import { Banner } from '@shared/components/ui/banner/banner';
 
 @Component({
   selector: 'app-about',
-  imports: [Aboutus, Banner],
+  imports: [Aboutus],
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
